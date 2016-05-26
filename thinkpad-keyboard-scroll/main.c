c16fbba7b86405e7bf2810f3b25f203709137e44
Nico Blodow <blodow@nicos-mbp.lan>
2016-05-26 21:18:12 +0200

    Initial Commit

---
+++ thinkpad-keyboard-scroll/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  thinkpad-keyboard-scroll
+//
+//  Created by Nico Blodow on 26/05/16.
+//  Copyright © 2016 Nico Blodow. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
